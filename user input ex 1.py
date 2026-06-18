6db45dc8b12af3e929fd3b5c8c34170b02527a57
kernel-pwn <adeeb1229@gmail.com>
2026-06-18 19:38:44 +0600

    changed code

--- user input ex 1.py
+++ user input ex 1.py
@@ -1,8 +1,8 @@
 # shopping cart program
 
 item = input("what item would you like?: ")
-price = float(input("what is the price?: "))
-quantity = int(input("how many items would you like?: "))
+price = float(input("what is the price?: $"))
+quantity = int(input(f"how many {item}/s would you like?: "))
 total_price = price * quantity
 
 print(f"U hv bought {quantity} {item}/s")
